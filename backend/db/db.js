3c217b7b9e9f31d4c13337e1b00246147b92d001
Kuldeep <deepanshukashyap388@gmail.com>
2024-09-19 02:45:46 +0530

    endpoint fix

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,6 +1,25 @@
 // db.js
 let users = [];
-const complaints = []; // New array to store complaints
+const complaints = [{
+  fullName : "Luffy",
+  email : "luffy@gmail.com",
+  password : "12345",
+  phoneNumber : 1234567890,
+  address : "East Blue",
+  idVerification : 12345,
+  emergencyContact : 12345,
+  agreement : true
+},
+{
+  fullName : "John Doe",
+  email : "john@gmail.com",
+  password : "123456",
+  phoneNumber : 1234567890,
+  address : "East Blue",
+  idVerification : 12345,
+  emergencyContact : 12345,
+  agreement : true
+}]; // New array to store complaints
 
 const addUser = (user) => {
   users.push(user);
